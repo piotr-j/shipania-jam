83f1ad9a85c9647921786aa051734ceb6897de75
PiotrJ <me@piotrjastrzebski.io>
2021-09-26 22:34:28 +0200

    fixed typo

--- core/src/main/java/io/piotrjastrzebski/psm/entities/DoorEntity.java
+++ core/src/main/java/io/piotrjastrzebski/psm/entities/DoorEntity.java
@@ -26,7 +26,7 @@ public class DoorEntity extends BaseEntity {
         Filter filterData = fixture.getFilterData();
         filterData.categoryBits = CATEGORY_WALL;
         filterData.maskBits = CATEGORY_PLAYER | CATEGORY_ENEMY | CATEGORY_PROJECTILE_PLAYER | CATEGORY_PROJECTILE_ENEMY;
-        filterData.set(filterData);
+        fixture.setFilterData(filterData);
 
         fixture.setSensor(true);
 
